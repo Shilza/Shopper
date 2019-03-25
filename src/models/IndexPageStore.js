245620e334cb94ff1ca53376cf257b59f0bfb312
Shilza <andgalatinov@gmail.com>
2019-03-25 10:39:07 +0300

    PageStore fixes

--- src/models/IndexPageStore.js
+++ src/models/IndexPageStore.js
@@ -7,9 +7,17 @@ import delay from "../utils/delay";
 class IndexPageStore {
     @observable isHoverListItem: boolean = false;
 
-    @observable isHoverMenu: boolean = false;
+    @observable isMenuShown: boolean = false;
 
     @observable displayedComponent: Element;
+
+    @action setIsMenuShown(value: boolean) {
+        this.isMenuShown = value;
+    }
+
+    @action setDisplayedComponent(component: Element) {
+        this.displayedComponent = component;
+    }
 
     @action setIsHovered(value: boolean) {
         delay(100).then(() => {this.isHoverListItem = value});
