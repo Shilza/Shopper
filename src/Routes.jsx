544346257546c983a75e68ebfd0e5ce6bf9f4e4b
Shilza <andgalatinov@gmail.com>
2019-03-25 10:37:25 +0300

    Add code splitting by routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
+import { Redirect } from 'react-router';
 import Header from './components/Header/Header';
+import SubHeader from './components/SubHeader/SubHeader';
 import Footer from './components/Footer/Footer';
-import Index from './pages/Index/Index';
 
-const NoMatch = () => <div>404</div>;
+const Product = React.lazy(() => import('./pages/Product/Product'));
+const New = React.lazy(() => import('./pages/New/New'));
+const Men = React.lazy(() => import('./pages/Index/Men'));
+const Women = React.lazy(() => import('./pages/Index/Women'));
+const NoMatch = React.lazy(() => import('./pages/NoMatch/NoMatch'));
+const WishList = React.lazy(() => import('./pages/WishList/WishList'));
 
 const Routes = () => (
     <Router>
         <>
             <Header />
-            <Switch>
-                <Route path="/" exact component={Index} />
-                <Route component={NoMatch} />
-            </Switch>
+            <MediaQuery minWidth={769}>
+                <SubHeader />
+            </MediaQuery>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/women" exact component={Women} />
+                    <Route path="/men" exact component={Men} />
+                    <Route path="/new" exact component={New} />
+                    <Route path="/wish-list" exact component={WishList} />
+                    <Route path="/product/:id" exact component={Product} />
+                    <Redirect exact from="/" to="/women" />
+                    <Route component={NoMatch} />
+                </Switch>
+            </Suspense>
             <Footer />
         </>
     </Router>
